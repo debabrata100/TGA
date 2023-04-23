570c8153b70988ecb91cbef6a36605261aa660e2
Debabrata <debabrata.nayak@publicissapient.com>
2023-04-23 20:42:36 +0530

    Mutation and Subscription Implemented

--- graphql-job-board-demo/resolvers.js
+++ graphql-job-board-demo/resolvers.js
@@ -1,4 +1,11 @@
 import { Job, Company } from "./db.js";
+import { PubSub } from "graphql-subscriptions";
+
+const pubSub = new PubSub();
+const COMPANY_ADDED = "COMPANY_ADDED";
+const JOB_ADDED = "JOB_ADDED";
+const COMPANY_REMOVED = "COMPANY_REMOVED";
+const JOB_REMOVED = "JOB_REMOVED";
 
 export const resolvers = {
   Query: {
@@ -15,6 +22,51 @@ export const resolvers = {
     company: async (job) => {
       console.log(job);
       return Company.findById(job.companyId);
+    },
+  },
+  Mutation: {
+    addCompany: async (_, { input }) => {
+      const { name, description } = input;
+      const company = await Company.create({
+        name,
+        description,
+      });
+      pubSub.publish(COMPANY_ADDED, { companyAdded: company });
+      return company;
+    },
+    deleteCompany: async (_, { id }) => {
+      const company = await Company.delete(id);
+      pubSub.publish(COMPANY_REMOVED, { companyRemoved: company });
+      return company;
+    },
+    addJob: async (_, { input }) => {
+      const { companyId, title, description } = input;
+      const job = await Job.create({
+        companyId,
+        title,
+        description,
+      });
+      pubSub.publish(JOB_ADDED, { jobAdded: job });
+      return job;
+    },
+    deleteJob: async (_, { id }) => {
+      const job = await Job.delete(id);
+      pubSub.publish(JOB_REMOVED, { jobRemoved: job });
+      return job;
+    },
+  },
+  Subscription: {
+    companyAdded: {
+      subscribe: () => pubSub.asyncIterator([COMPANY_ADDED]),
+    },
+    jobAdded: {
+      subscribe: () => pubSub.asyncIterator([JOB_ADDED]),
+    },
+    companyRemoved: {
+      subscribe: () => pubSub.asyncIterator([COMPANY_REMOVED]),
+    },
+    jobRemoved: {
+      subscribe: () => pubSub.asyncIterator([JOB_REMOVED]),
     },
   },
 };
