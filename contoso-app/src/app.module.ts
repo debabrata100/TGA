ff207aae00cc7f71e9449e4d00f72d239dd69eb1
Debabrata <m.debabrata100@gmail.com>
2023-05-15 06:26:33 +0530

    Added neo4j for db configuration

--- contoso-app/src/app.module.ts
+++ contoso-app/src/app.module.ts
@@ -1,10 +1,21 @@
+import { Neo4jModule } from '@dbc-tech/nest-neo4j';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './products/product.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    ProductModule,
+    Neo4jModule.forRoot({
+      scheme: 'bolt',
+      host: 'localhost',
+      port: 'bolt://localhost:7687',
+      username: 'neo4j',
+      password: 'Pass@123',
+      disableLosslessIntegers: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
